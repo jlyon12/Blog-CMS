16d5106a78d6118bb5780a2249e16c826b10990a
Jarrett Lyon <64499632+probableactions@users.noreply.github.com>
2023-10-23 19:13:37 -0600

    install: sass
    copy sass folder from main client for consistency

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Router from 'src/router';
+import 'src/sass/index.scss';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<Router />
