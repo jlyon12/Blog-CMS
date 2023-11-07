21cb9b83b7549dc0db9eba3f5066a6dfcd3966fb
Jarrett Lyon <64499632+probableactions@users.noreply.github.com>
2023-11-06 21:30:37 -0700

    ux: add scroll to top util

--- src/layouts/PageLayout.jsx
+++ src/layouts/PageLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router';
 import useDarkModeContext from 'src/hooks/useDarkModeContext';
+import ScrollToTop from 'src/utils/ScrollToTop';
 import Header from 'src/components/Header/Header';
 import ConfirmModal from 'src/components/ConfirmModal/ConfirmModal';
 import styles from './PageLayout.module.scss';
@@ -7,6 +8,7 @@ const PageLayout = () => {
 	const { darkMode } = useDarkModeContext();
 	return (
 		<div className={darkMode ? `theme-dark` : 'theme-light'}>
+			<ScrollToTop />
 			<div className={styles.app}>
 				<Header />
 				<Outlet />
